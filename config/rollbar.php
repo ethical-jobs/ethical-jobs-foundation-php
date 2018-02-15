9289165e0ecb579bd9ca279658d6211a27506113
andrewmclagan <andrew@ethicaljobs.com.au>
2018-02-15 21:38:51 +0200

    Adding new reollbar configs

--- config/rollbar.php
+++ config/rollbar.php
@@ -2,7 +2,8 @@
 
 return [
 	'rollbar' => [
-    	'access_token' 	=> env('ROLLBAR_TOKEN'),
-    	'level' 		=> env('ROLLBAR_LEVEL'),
+    	'access_token' 				=> env('ROLLBAR_TOKEN'),
+    	'level' 					=> 'info',
+    	'enable_utf8_sanitization'  => false,
 	],
 ];
